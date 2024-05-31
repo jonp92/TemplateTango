18ebb433b8f0b6e5c437228e3d4a30726a9fc3d3
jonathan <jonathanpressler@icloud.com>
2024-05-31 09:39:51 -0400

    chore: Update .gitignore and TemplateTango.h

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,16 +1,16 @@
 #include <Arduino.h>
-#include <TemplateTango.h>
+#include "TemplateTango.h"
 
 // Functions to simulate sensor readings
-string readTemperature() { return "25.5"; }
-string readSensor1() { return "100.1"; }
-string readSensor2() { return "200.2"; }
+String readTemperature() { return "25.5"; }
+String readSensor1() { return "100.1"; }
+String readSensor2() { return "200.2"; }
 
 void setup() {
     Serial.begin(9600);
 
     // Define the template
-    string templateStr = R"(
+    String templateStr = R"(
         <!DOCTYPE html>
         <html>
         <head>
@@ -27,15 +27,15 @@ void setup() {
     )";
 
     // Collect variable values
-    map<string, string> variables = {
+    std::map<String, String> variables = {
         {"temperature", readTemperature()},
         {"sensor1_reading", readSensor1()},
         {"sensor2_reading", readSensor2()}
     };
 
     // Render the template
-    string renderedHtml = TemplateTango::render(templateStr, variables);
-    Serial.println(renderedHtml.c_str());
+    String renderedHtml = TemplateTango::render(templateStr, variables);
+    Serial.println(renderedHtml);
 }
 
 void loop() {
